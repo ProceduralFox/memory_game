89d551ce35297fc6a2abdaeebdb3878a9a621133
Amir Al Khatib <amir.al.khatib.z@gmail.com>
2021-10-07 21:05:08 +0200

    added the start game page and the 6x6 monitor layout

--- src/components/memory_game.jsx
+++ src/components/memory_game.jsx
@@ -39,7 +39,7 @@ const Memory_game = () => {
         setMoves(0)
     }
 
-    const click = () => {
+    const game_click = () => {
         if(active){
             
         } else {
@@ -47,27 +47,72 @@ const Memory_game = () => {
         }
     }
 
-    const [cards, setCards] = useState(generate(4))
+    const settings_click = (id, value) => {
+        const temp = [...settings]
+        temp[id] = value
+        setSettings(temp)
+    }
+
+    const [cards, setCards] = useState(generate(6))
     const [test, setTest] = useState()
     const [active, setActive] = useState()
     const [moves, setMoves] = useState(0)
     const [solved, setSolved] = useState([])
+    const [started, setStarted] = useState(false)
+    const [settings, setSettings] = useState([5, 5, 5, 5])
 
+    if(!started){
+        return (
+        <div className="start_page">
+            <div className="start_content">
+                <div className="start_title">
+                    memory
+                </div>
+                <div className="start_menu">
+
+                    <div className="start_menu_item">
+                        <button onClick={()=>{settings_click(0, 0)}} className={`${settings[0]=== 0 ? "menu_selection_clicked" : "menu_selection"}`}>Numbers</button>
+
+                        <button onClick={()=>{settings_click(0, 1)}} className={`${settings[0]=== 1 ? "menu_selection_clicked" : "menu_selection"}`}>Icons</button>
+                    </div>
+
+                    <div className="start_menu_item">
+                        <button onClick={()=>{settings_click(1, 0)}} className={`${settings[1]=== 0 ? "menu_selection_clicked" : "menu_selection"}`}>1</button>
+
+                        <button onClick={()=>{settings_click(1, 1)}} className={`${settings[1]=== 1 ? "menu_selection_clicked" : "menu_selection"}`}>2</button>
+                        <button onClick={()=>{settings_click(1, 2)}} className={`${settings[1]=== 2 ? "menu_selection_clicked" : "menu_selection"}`}>3</button>
+                        <button onClick={()=>{settings_click(1, 3)}} className={`${settings[1]=== 3 ? "menu_selection_clicked" : "menu_selection"}`}>4</button>
+
+                    </div>
+
+                    <div className="start_menu_item">
+                        <button onClick={()=>{settings_click(2, 4)}} className={`${settings[2]=== 4 ? "menu_selection_clicked" : "menu_selection"}`}>4x4</button>
+
+                        <button onClick={()=>{settings_click(2, 6)}} className={`${settings[2]=== 6 ? "menu_selection_clicked" : "menu_selection"}`}>6x6</button>
+                    </div>
+
+                    <button onClick={()=>{
+                        setCards(generate(settings[2]))
+                        setStarted(true)}}className="btn_big">Start Game</button>
+                </div>
+            </div>
+        </div> )
+    }
 
     return (
     <div className="body">
-        <div className="upper4">
+        <div className={`upper4`}>
             <div className="name4">memory</div>
             <div className="utilities4">
                 <button onClick={()=>shuffle()} className="btn_primary">Restart</button>
-                <button className="btn_secondary">New Game</button>
+                <button onClick={()=>{setStarted(false)}}className="btn_secondary">New Game</button>
             </div>
         </div>
-        <div className="grid4">
+        <div className={`grid${settings[2]}`}>
             {cards.map((card, index)=>{
 
                 if(card[1]==1){
-                    return <div className="solved card4">
+                    return <div className={`solved card${settings[2]}`}>
                         {card[0]}
                     </div>
                 }
@@ -76,7 +121,7 @@ const Memory_game = () => {
 
                 return(
                     <div key={index} 
-                        className={`${active===index ? 'clicked':'unclicked'} card4`}
+                        className={`${active===index ? 'clicked':'unclicked'} card${settings[2]}`}
                         onClick={()=>{
                             if(active){
                                 const match = cards[active][0] === card[0]
