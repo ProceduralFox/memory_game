4683a1b019eca39d4ab0340762558d7fe96167a3
Amir Al Khatib <amir.al.khatib.z@gmail.com>
2022-01-05 13:40:56 +0100

    forgot to save one file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-import Memory_game from './components/memory_game.jsx';
+import MemoryGame from './components/memory_game.jsx';
 
 
 function App() {
   return (
     <div className="App">
-      <Memory_game/>
+      <MemoryGame/>
     </div>
   );
 }
